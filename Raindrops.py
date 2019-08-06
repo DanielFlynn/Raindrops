13a337beb6d315e130079b5aad2010612d335eaa
Daniel Flynn <dflynn@spartaglobal.com>
2019-08-06 10:14:01 +0100

    Created Raindrops.py & Raindrops_Unittest.py

---
+++ Raindrops.py
@@ -0,0 +1,11 @@
+def raindrops(number):
+    statement = ''
+    if number % 3 == 0:
+        statement += 'Pling'
+    if number % 5 == 0:
+        statement += 'Plang'
+    if number % 7 == 0:
+        statement += 'Plong'
+    if len(statement) == 0:
+        statement += str(number)
+    return statement
